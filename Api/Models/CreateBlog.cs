30d20eb19b8fed1376b26989a2ea1caaa674e562
akshat singh <s.akshat6262@gmail.com>
2022-03-06 15:43:42 +0530

    First commit

---
+++ Api/Models/CreateBlog.cs
@@ -0,0 +1,10 @@
+﻿namespace Api.Models
+{
+    public class CreateBlog
+    {
+        public string title { get; set; }
+        public string description { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+    }
+}
